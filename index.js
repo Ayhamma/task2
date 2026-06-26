9f625a9e8a62e5484b64b7cfbacb61e36696713c
Ayhamma <144667767+Ayhamma@users.noreply.github.com>
2026-06-26 13:15:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ app.get("/login/", (req, res) => {
   res.send("ayham");
 });
 
-function task(x) {
-  return x * x;
-}
-
-app.get("/sample/", (req, res) => {
+app.get("/sample/:x/", (req, res) => {
+  const x = Number(req.params.x);
   res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-  res.send(task.toString());
+  res.send(String(x * x));
 });
 
 const PORT = process.env.PORT || 3000;
