a2ad4c529f9117259a1c92b8defdbf0b916dc55e
Ayhamma <144667767+Ayhamma@users.noreply.github.com>
2026-06-26 14:29:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import bodyParser from "body-parser";
 import { createReadStream } from "fs";
 import crypto from "crypto";
 import http from "http";
+import { MongoClient } from "mongodb";
 import appSrc from "./app.js";
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, MongoClient);
 
 app.listen(process.env.PORT);
