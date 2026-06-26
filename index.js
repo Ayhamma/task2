99cb2cc205ae0363dd4bf347026d93e392186095
Ayhamma <144667767+Ayhamma@users.noreply.github.com>
2026-06-26 13:25:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import appSrc from "./app.js";
 
-const app = express();
+const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-app.use((req, res, next) => {
-  if (req.path !== "/" && !req.path.endsWith("/")) {
-    return res.redirect(301, req.path + "/");
-  }
-  next();
-});
-
-app.get("/login/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-  res.send("ayham");
-});
-
-function task(x) {
-  return new Promise((resolve, reject) => {
-    if (x < 18) {
-      resolve("yes");
-    } else {
-      reject("no");
-    }
-  });
-}
-
-app.get("/promise/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-  res.send(task.toString());
-});
-
-app.get("/fetch/", (req, res) => {
-  res.setHeader("Content-Type", "text/html; charset=UTF-8");
-  res.send(`
-<!DOCTYPE html>
-<html>
-<body>
-<input id="inp">
-<button id="bt">OK</button>
-
-<script>
-document.getElementById("bt").onclick = async function () {
-  const inp = document.getElementById("inp");
-  const response = await fetch(inp.value);
-  const text = await response.text();
-  inp.value = text;
-};
-</script>
-</body>
-</html>
-`);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(process.env.PORT);
